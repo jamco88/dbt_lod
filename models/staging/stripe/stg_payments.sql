ad5879198ae8cb70f0628d8aec90cc62ab9e54f2
James Cooper <papillionaire@gmail.com>
2021-08-12 11:10:38 +0000

    used cents_to_dollars in stg_payments.sql

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ with payments as (select
     orderid as order_id,
     paymentmethod as payment_method,
     status as order_status,
-    amount/100 as amount,
+    {{ cents_to_dollars('amount', 2) }} as amount,
     created as created,
     date_trunc('second', _batched_at) as batched_at
 from
